0955ce67dccc38b7ebfc1757cfaabf733587dabe
Aidan <aidsc@bu.edu>
2024-04-23 19:48:15 -0400

    added pagination and reversed to match github

--- src/components/GradeTable.tsx
+++ src/components/GradeTable.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination} from '@mui/material';
 import { IGrade, IUniversityClass } from '../types/api_types';
 
 interface GradeTableProps {
@@ -8,10 +8,30 @@ interface GradeTableProps {
 }
 
 export const GradeTable: React.FC<GradeTableProps> = ({ grades, classList }) => {
-  const findClassNameById = (classId: string): string => {
-    const classObj = classList.find((c) => c.classId === classId);
-    return classObj ? classObj.title : "Unknown";
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
   };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const findClassNameById = (classId: string): string => {
+    const classObj = classList.find(c => c.classId === classId);
+    return classObj ? classObj.title : 'Unknown';
+  };
+
+  const reversedGrades = [...grades].reverse();
+  const gradesToDisplay = reversedGrades.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
 
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
@@ -28,19 +48,28 @@ export const GradeTable: React.FC<GradeTableProps> = ({ grades, classList }) => 
             </TableRow>
           </TableHead>
           <TableBody>
-            {grades.map((gradeData) => (
-              <TableRow key={gradeData.studentId}>
-                <TableCell>{gradeData.studentId}</TableCell>
-                <TableCell>{gradeData.name}</TableCell>
-                <TableCell>{gradeData.classId}</TableCell>
-                <TableCell>{findClassNameById(gradeData.classId)}</TableCell>
-                <TableCell>{"fall2022"}</TableCell>
-                <TableCell>{gradeData.finalGrade}</TableCell>
-              </TableRow>
+            {gradesToDisplay.map((gradeData) => (
+                <TableRow key={gradeData.studentId}>
+                  <TableCell>{gradeData.studentId}</TableCell>
+                  <TableCell>{gradeData.name}</TableCell>
+                  <TableCell>{gradeData.classId}</TableCell>
+                  <TableCell>{findClassNameById(gradeData.classId)}</TableCell>
+                  <TableCell>{"fall2022"}</TableCell>
+                  <TableCell>{gradeData.finalGrade}</TableCell>
+                </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 100]}
+        component="div"
+        count={grades.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </Paper>
   );
 };
